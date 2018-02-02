e606b3a5fa7ed932638f4f2491c745781567500e
Emp-Norton <jamesjelenko@gmail.com>
2018-02-01 16:37:31 -0800

    clean up comments, notes on how to incoporate this all into one script rather than calling it twice

--- test/dataGenerator.js
+++ test/dataGenerator.js
@@ -1,24 +1,18 @@
 'use strict'
-
-
 let fs = require('fs');
-
-
 
 
 let createFictitiousObjects = (startIndex, limit, batchName) => {
 
+	let bpmOptions = [45, 60, 90, 120, 150];
+	let songs = [];
+	let keywords = ["featured", "hip-hop", "reggae", "funk", "bluegrass", "blues", "rock"]
 
+	for (let songIdIndex = 1; songIdIndex < 1000; songIdIndex++) { // create 1000 fake song objects
+		let bpmIndex = Math.floor(Math.random() * bpmOptions.length); // randomly select bpm property from options array
+		songs.push({songId: songIdIndex, bpm: bpmOptions[bpmIndex]});
+	}
 
-//let generateStart = Date.now() // data creation start time 
-let bpmOptions = [45, 60, 90, 120, 150];
-let songs = [];
-let keywords = ["featured", "hip-hop", "reggae", "funk", "bluegrass", "blues", "rock"]
-
-for (let songIdIndex = 1; songIdIndex < 1000; songIdIndex++) { // create 1000 fake song objects
-	let bpmIndex = Math.floor(Math.random() * bpmOptions.length); // randomly select bpm property from options array
-	songs.push({songId: songIdIndex, bpm: bpmOptions[bpmIndex]});
-}
 	let entries = [];
 	for (let i = startIndex; i < limit; i++) { 
 		let fakeObj = {
@@ -49,23 +43,24 @@ for (let songIdIndex = 1; songIdIndex < 1000; songIdIndex++) { // create 1000 fa
 
 
 	
-
+// create write stream pointing to CSV file on external HD
 	let writeTo = fs.createWriteStream(`../../../../../../media/norton/Seagate\ Backup\ Plus\ Drive/Coding\ Stuff/storage/${batchName}.csv`);
-	// find EXHD path: ../../../../../../media/norton/'Seagate Backup Plus Drive'/'Coding Stuff'/storage/playlistData.csv` - path correct for CD
+	
 
 	entries.forEach((entry, index) => {
 		if (index % 10000 == 0) console.log('Writing entry #' + index + ' of ' + batchName + ' batch.')
 		writeTo.write(JSON.stringify(entry));
 	})
-// let generateFinish = Date.now()
 
 	console.log('Finished writing entries to CSV.')
-  //let generateTime = Math.abs(generateFinish - generateStart) / 1000;
-	//console.log('Generated ' + entries.length + ' in ' + generateTime + ' seconds.');
+	return
 }
 
 
-// first set (running these all at once causes a crash -- out of memory at 6.67 million (67% of way through 6th batch)). Maybe promisfy?
+// first set (running all 10 at once causes a memory crash at 6.67 million (67% of way through 6th batch)). 
+// also, trying to run too large a range (0 through > 1.8 million) causes a crash -- out of memory in both cases.
+// maybe I can setup a sequence of promises, i.e. don't invoke "second" until "first" has finished, or (more hacky) 
+// run a bash or python script that calls this function then sleeps for 1 minute (repeated 10 times with appropriate arguments)
 
 // createFictitiousObjects(0, 1000000, "first")
 // createFictitiousObjects(1000000, 2000000, "second")
@@ -74,11 +69,11 @@ for (let songIdIndex = 1; songIdIndex < 1000; songIdIndex++) { // create 1000 fa
 // createFictitiousObjects(4000000, 5000000, "fifth")
 
 // second set 
-createFictitiousObjects(5000000, 6000000, "sixth")
-createFictitiousObjects(6000000, 7000000, "seventh")
-createFictitiousObjects(7000000, 8000000, "eigth")
-createFictitiousObjects(8000000, 9000000, "ninth")
-createFictitiousObjects(9000000, 10000000, "tenth")
+// createFictitiousObjects(5000000, 6000000, "sixth")
+// createFictitiousObjects(6000000, 7000000, "seventh")
+// createFictitiousObjects(7000000, 8000000, "eigth")
+// createFictitiousObjects(8000000, 9000000, "ninth")
+// createFictitiousObjects(9000000, 10000000, "tenth")
 
 
 module.exports.createFictitiousObjects = createFictitiousObjects;
