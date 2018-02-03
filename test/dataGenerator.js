aa76716228da78054435a21bad66b162757fb344
Emp-Norton <jamesjelenko@gmail.com>
2018-02-03 12:20:02 -0800

    rename fields to match DB schema

--- test/dataGenerator.js
+++ test/dataGenerator.js
@@ -5,7 +5,7 @@ let fs = require('fs');
 let createFictitiousObjects = (startIndex, limit, batchName) => {
 
 	let bpmOptions = [45, 60, 90, 120, 150];
-	let songs = [];
+	let songs = []; // the song creator code should probably be outside this function -- if I leave it here it gets overwritten and two songs can have the same ID.
 	let keywords = ["featured", "hip-hop", "reggae", "funk", "bluegrass", "blues", "rock"]
 
 	for (let songIdIndex = 1; songIdIndex < 1000; songIdIndex++) { // create 1000 fake song objects
@@ -16,8 +16,8 @@ let createFictitiousObjects = (startIndex, limit, batchName) => {
 	let entries = [];
 	for (let i = startIndex; i < limit; i++) { 
 		let fakeObj = {
-			entryNumber: i,
-			entryName: `Playlist${i}`,
+			playlistId: i,
+			playlistName: `Playlist${i}`,
 			songList:[],
 			shuffledQueue: [],
 			keywords: []
@@ -41,11 +41,9 @@ let createFictitiousObjects = (startIndex, limit, batchName) => {
 		entries.push(fakeObj)
 	}
 
-
-	
-// create write stream pointing to CSV file on external HD
+	// create write stream pointing to CSV file on external HD
+	// let writeTo = fs.createWriteStream(`../../../../../../media/norton/Seagate\ Backup\ Plus\ Drive/Coding\ Stuff/storage/${batchName}.csv`);
 	let writeTo = fs.createWriteStream(`../../../../../../media/norton/Seagate\ Backup\ Plus\ Drive/Coding\ Stuff/storage/${batchName}.csv`);
-	
 
 	entries.forEach((entry, index) => {
 		if (index % 10000 == 0) console.log('Writing entry #' + index + ' of ' + batchName + ' batch.')
@@ -56,27 +54,33 @@ let createFictitiousObjects = (startIndex, limit, batchName) => {
 	return
 }
 
-
 // first set (running all 10 at once causes a memory crash at 6.67 million (67% of way through 6th batch)). 
 // also, trying to run too large a range (0 through > 1.8 million) causes a crash -- out of memory in both cases.
 // maybe I can setup a sequence of promises, i.e. don't invoke "second" until "first" has finished, or (more hacky) 
 // run a bash or python script that calls this function then sleeps for 1 minute (repeated 10 times with appropriate arguments)
+// let executeFirstBatch = () => {
+	//createFictitiousObjects(0, 1000000, "playlists_1")
+	//createFictitiousObjects(1000000, 2000000, "playlists_2")
+	// createFictitiousObjects(2000000, 3000000, "playlists_3")
+	 createFictitiousObjects(3000000, 4000000, "playlists_4")
+	// createFictitiousObjects(4000000, 5000000, "playlists_5")
+// }
 
-// createFictitiousObjects(0, 1000000, "first")
-// createFictitiousObjects(1000000, 2000000, "second")
-// createFictitiousObjects(2000000, 3000000, "third")
-// createFictitiousObjects(3000000, 4000000, "fourth")
-// createFictitiousObjects(4000000, 5000000, "fifth")
-
-// second set 
-// createFictitiousObjects(5000000, 6000000, "sixth")
-// createFictitiousObjects(6000000, 7000000, "seventh")
-// createFictitiousObjects(7000000, 8000000, "eigth")
-// createFictitiousObjects(8000000, 9000000, "ninth")
-// createFictitiousObjects(9000000, 10000000, "tenth")
+// // second set 
+// let executeSecondBatch = () => {
+// 	createFictitiousObjects(5000000, 6000000, "playlists_6")
+// 	createFictitiousObjects(6000000, 7000000, "playlists_7")
+// 	createFictitiousObjects(7000000, 8000000, "playlists_8")
+// 	createFictitiousObjects(8000000, 9000000, "playlists_9")
+// 	createFictitiousObjects(9000000, 10000000, "playlists_10")
+// }
 
 
-module.exports.createFictitiousObjects = createFictitiousObjects;
+module.exports = {
+	// createFictitiousObjects,
+	// executeSecondBath, 
+	// executeFirstBatch
+}
 
 
 
